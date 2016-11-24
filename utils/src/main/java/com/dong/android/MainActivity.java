80be16a7053e6a2d196e442bcb9bcee964ad1c48
DongZhi1989 <1060221762@qq.com>
2016-11-24 16:53:17 +0800

    add test

--- utils/src/main/java/com/dong/android/MainActivity.java
+++ utils/src/main/java/com/dong/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dong.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dong.android.utils.LogUtils;
+import com.dong.android.utils.UIUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test();
     }
+
+    private void test() {
+        LogUtils.e("test");
+        UIUtils.showToast("test");
+        UIUtils.showToast(R.string.test_toast);
+    }
+
 }
