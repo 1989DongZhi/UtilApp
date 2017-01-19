87f2c72563981129ec0e6651429ed91813c2e851
DongZhi1989 <1060221762@qq.com>
2017-01-19 14:20:14 +0800

    add android native activity

---
+++ utils/src/main/java/com/dong/android/ui/gnative/TestEmptyActivity.java
@@ -0,0 +1,15 @@
+package com.dong.android.ui.gnative;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dong.android.R;
+
+public class TestEmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_empty);
+    }
+}
