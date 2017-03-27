99e60093fde5e12c9f2e505a03a4db4d504ba083
DongZhi1989 <1060221762@qq.com>
2017-03-27 13:48:39 +0800

    fix LogUtils output display class name and method name

--- utils/src/main/java/com/dong/android/utils/LogUtils.java
+++ utils/src/main/java/com/dong/android/utils/LogUtils.java
@@ -2,6 +2,8 @@ package com.dong.android.utils;
 
 import android.util.Log;
 
+import com.dong.android.app.AppManager;
+
 /**
  * 作者：<Dr_dong>
  * 日期：2016/11/24.
@@ -14,8 +16,6 @@ public class LogUtils {
      * 调试开关,日志控制,默认打开,发布时关闭
      */
     public static boolean DEBUG = true;
-    private static String TAG = "===>" + Thread.currentThread().getStackTrace()[3].getClassName() +
-            "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>";
 
     public static void setLogDebug(boolean logDebug) {
         DEBUG = logDebug;
@@ -23,7 +23,10 @@ public class LogUtils {
 
     public static void e(String msg) {
         if (DEBUG)
-            e(TAG, msg);
+            e("===>" + Thread.currentThread().getStackTrace()[3].getClassName().replace(AppManager.PACKAGE_NAME + ".", "") +
+                            "===>" + Thread.currentThread().getStackTrace()[3].getMethodName() +
+                            "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>"
+                    , msg);
     }
 
     public static void e(String tag, String msg) {
@@ -33,7 +36,9 @@ public class LogUtils {
 
     public static void w(String msg) {
         if (DEBUG)
-            w(TAG, msg);
+            w("===>" + Thread.currentThread().getStackTrace()[3].getClassName().replace(AppManager.PACKAGE_NAME + ".", "") +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getMethodName() +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>", msg);
     }
 
     public static void w(String tag, String msg) {
@@ -43,7 +48,9 @@ public class LogUtils {
 
     public static void i(String msg) {
         if (DEBUG)
-            i(TAG, msg);
+            i("===>" + Thread.currentThread().getStackTrace()[3].getClassName().replace(AppManager.PACKAGE_NAME + ".", "") +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getMethodName() +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>", msg);
     }
 
     public static void i(String tag, String msg) {
@@ -53,7 +60,9 @@ public class LogUtils {
 
     public static void d(String msg) {
         if (DEBUG)
-            d(TAG, msg);
+            d("===>" + Thread.currentThread().getStackTrace()[3].getClassName().replace(AppManager.PACKAGE_NAME + ".", "") +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getMethodName() +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>", msg);
     }
 
     public static void d(String tag, String msg) {
@@ -63,11 +72,14 @@ public class LogUtils {
 
     public static void v(String msg) {
         if (DEBUG)
-            v(TAG, msg);
+            v("===>" + Thread.currentThread().getStackTrace()[3].getClassName().replace(AppManager.PACKAGE_NAME + ".", "") +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getMethodName() +
+                    "===>" + Thread.currentThread().getStackTrace()[3].getLineNumber() + "===>", msg);
     }
 
     public static void v(String tag, String msg) {
-        Log.v(tag, msg);
+        if (DEBUG)
+            Log.v(tag, msg);
     }
 
 }
