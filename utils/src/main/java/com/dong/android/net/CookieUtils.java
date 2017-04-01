6f2b716a932a2e43c2350ca8e7d6fe010ac62fff
DongZhi1989 <1060221762@qq.com>
2017-04-01 16:27:51 +0800

    add : Add net class

---
+++ utils/src/main/java/com/dong/android/net/CookieUtils.java
@@ -0,0 +1,42 @@
+package com.dong.android.net;
+
+import com.dong.android.utils.preferences.PreferencesUtils;
+
+/**
+ * @author <dr_dong>
+ * @time 2017/4/1 11:27
+ */
+public class CookieUtils {
+
+    private static final String JSESSIONID_KEY = "jsessionid";
+    private static final String DOMAIN_KEY = "cookie_domain";
+    private static final String JSESSIONID_TIME = "jsessionidtime";
+    private static final long SIX_DAYS = 6 * 24 * 60 * 60 * 1000;
+
+    public static String getSessionIdFromPref() {
+        long jsessionid_time = PreferencesUtils.get(JSESSIONID_TIME, System.currentTimeMillis());
+        if ((System.currentTimeMillis() - jsessionid_time) > SIX_DAYS) {
+            return "";
+        }
+        return PreferencesUtils.get(JSESSIONID_KEY, "");
+    }
+
+    public static void addSessionIdToPref(String jsessionid) {
+        PreferencesUtils.put(JSESSIONID_KEY, jsessionid);
+        PreferencesUtils.put(JSESSIONID_TIME, System.currentTimeMillis());
+    }
+
+    public static String getDomainFromPref() {
+        long jsessionid_time = PreferencesUtils.get(JSESSIONID_TIME, System.currentTimeMillis());
+        if ((System.currentTimeMillis() - jsessionid_time) > SIX_DAYS) {
+            return "";
+        }
+        return PreferencesUtils.get(DOMAIN_KEY, "");
+    }
+
+    public static void addDomainToPref(String domain) {
+        PreferencesUtils.put(DOMAIN_KEY, domain);
+        PreferencesUtils.put(JSESSIONID_TIME, System.currentTimeMillis());
+    }
+
+}
---
+++ utils/src/main/java/com/dong/android/net/ServerApi.java
@@ -0,0 +1,17 @@
+package com.dong.android.net;
+
+/**
+ * @author <dr_dong>
+ * @time 2017/4/1 11:04
+ */
+public class ServerApi {
+
+    //微信服务器
+    public static final String WX_SERVER = "https://api.weixin.qq.com/";
+    private static final String LOGON_SUFFIX = "login";
+    private static final String LOGON_OUT_SUFFIX = "logout";
+    public static String SERVER = "http://http://eat-admin.dothinkings.com/";
+    // 登录
+    public static String LOGON = SERVER + LOGON_SUFFIX;
+    public static String LOGON_OUT = SERVER + LOGON_OUT_SUFFIX;
+}
